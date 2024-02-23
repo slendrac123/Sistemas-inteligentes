05d922ebc2afabab73bcaf6d5e8c6a7d1f4899ea
brrodriguezd <brrodriguezd@unal.edu.co>
2024-02-23 11:11:09 -0500

    creacion del proyecto

---
+++ tetris/src/main.py
@@ -0,0 +1,3 @@
+from settings import X, Y
+
+print(X,Y)
---
+++ tetris/src/mousedetection.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+file = open("settings.py","w")
+
+#Detecta la posición del mouse
+x,y = pyautogui.position()
+
+#la escribe a un archivo settings.py
+file.write("X = " + str(x) + "\nY = " +str(y))
+print(x,y)
