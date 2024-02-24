9339429653e0fd95129d0caa5cfed4db7586f81f
slendrac123 <jencisop@unal.edu.co>
2024-02-24 17:37:24 -0500

    Adición método determinar_pieza()

--- tetris/src/agente.py
+++ tetris/src/agente.py
@@ -11,10 +11,28 @@ class Agente:
         self.X=X
         self.Y=Y
     def determinar_pieza(self):
-        print( "X:{} Y: {} Pixel: {}".format(self.X, self.Y, pyscreeze.pixel(self.X , self.Y)))
+        
+        #print( "X:{} Y: {} Pixel: {}".format(self.X, self.Y, pyscreeze.pixel(self.X , self.Y)))
+        color = pyscreeze.pixel(self.X , self.Y)
+        
+        if color == (116, 255, 236):
+            return "I"
+        elif color == (236, 255, 116):
+            return "S"
+        elif color == (255, 118, 128):
+            return "Z"
+        elif color == (255, 188, 118):
+            return "L"
+        elif color == (154, 127, 255):
+            return "J"
+        elif color == (255, 128, 255):
+            return "T"
+        elif color == (255, 255, 117):
+            return "O"
+        
     def move(self):
         self.determinar_pieza()
     def compute(self):
         self.move()
-        print("computando")
+        #print("computando")
     
