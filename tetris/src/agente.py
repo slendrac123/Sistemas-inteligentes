30930b566006340b5464ce412fda61c1a8aefac1
brrodriguezd <brrodriguezd@unal.edu.co>
2024-03-15 10:53:27 -0500

    rangos y actualización del estado tablero con una matriz

--- tetris/src/agente.py
+++ tetris/src/agente.py
@@ -17,12 +17,13 @@ class Agente:
         self.Y = Y
         # iniciar el heap en 0 junto con el índice horizontal
         self.heap = MinHeap(10, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.estado_tablero = [0] * 10
+        self.altura_tablero = [0] * 10
+        self.estado_tablero = [[0]*10] * 20
         # revisa que la altura sea la correcta para no dejar huecos
 
     def is_move_possible(self, altitud):
         idx = 0
-        
+
         # las coordenadas estan en formato de array[array[tuplas]]
         for x in self.pieza.arr_coordenadas:
             terminado = True
@@ -48,7 +49,7 @@ class Agente:
                     if (altitud[1] + i[0] < 0):
                         raise IndexError
                     if (altitud[0] + i[1]
-                            != self.estado_tablero[altitud[1] + i[0]]):
+                            != self.altura_tablero[altitud[1] + i[0]]):
                         terminado = False
                         break
                     # añade las coordenadas
@@ -61,14 +62,24 @@ class Agente:
                 #    break
             if terminado:
                 # cuenta las coordenadas y frecuencia
+                for i in x:
+                    self.estado_tablero[altitud[1]+i[0]][altitud[0]+i[1]] = 1
                 for k in coord.keys():
                     # actualiza el tablero y el heap
                     # print ("k: "+ str(k) +" c: "+ str(c[k]))
                     # print("indice 1+k: " + str(altitud[1]+k))
-                    self.estado_tablero[altitud[1] + k] += coord[k]
+                    self.altura_tablero[altitud[1] + k] += coord[k]
                     self.heap.changePriority(
                         self.heap.getIndex(altitud[1] + k),
-                        self.estado_tablero[altitud[1] + k])
+                        self.altura_tablero[altitud[1] + k])
+                # check por si hay que disminuir las alturas
+                for i in self.estado_tablero:
+                    if 0 in i:
+                        continue
+                    self.estado_tablero.remove(i)
+                    self.estado_tablero.append([0] * 10)
+                    for j in self.heap.heap:
+                        j[0] -= 1
                 # por cosas de la rotación hay un index que anota cuantos giros
                 # a su vez prioriza los estados horizontales
                 # hace las rotaciones
@@ -115,10 +126,14 @@ class Agente:
             else:
                 # si no encuentra movimiento posible guarda la pieza
                 (x, y, z) = pyscreeze.pixel(720, 337)
-                if x < 50 and y < 50 and z < 50:
-                    return self.ultima_baza(self.heap.heap[0])
-                pyautogui.press(['c'])
-                return
+                print("pixel 720, 337" + str(pyscreeze.pixel(720, 337)))
+                (x, y, z) = pyscreeze.pixel(720, 337)
+                if (x in range(11, 51) and y in range(10, 51) and
+                        z in range(10, 51)):
+                    self.ultima_baza(self.heap.heap[0])
+                else:
+                    pyautogui.press(['c'])
+                    return
         # envia la pieza a donde la quiere ubicar
         # todas las piezas las tomo como si estuvieran en el punto 4 horizontal
         horizontal_mv = 4 - buff[1]
@@ -132,26 +147,34 @@ class Agente:
     def determinar_pieza(self):
         # print( "X:{} Y: {} Pixel: {}"
         # .format(self.X, self.Y, pyscreeze.pixel(self.X , self.Y)))
-        color = pyscreeze.pixel(self.X, self.Y)
-        if color in ((116, 255, 235), (80, 240, 185)):
+        (r, g, b) = pyscreeze.pixel(self.X, self.Y)
+        if (r in range(80, 117) and g in range(240, 256) and
+                b in range(185, 236)):
             print("pixel 720, 337" + str(pyscreeze.pixel(720, 337)))
             (x, y, z) = pyscreeze.pixel(720, 337)
-            if x < 50 and y < 50 and z < 50:
+            if (x in range(11, 51) and y in range(10, 51) and
+                    z in range(10, 51)):
                 return Piece('I')
             pyautogui.press('c')
             return self.determinar_pieza()
 
-        elif color in ((237, 255, 116), (181, 240, 78)):
+        elif (r in range(181, 238) and g in range(240, 256) and
+                b in range(78, 117)):
             return Piece('S')
-        elif color in ((255, 119, 130), (229, 72, 80)):
+        elif (r in range(229, 256) and g in range(229, 256) and
+                b in range(80, 131)):
             return Piece('Z')
-        elif color in ((255, 189, 118), (232, 134, 74)):
+        elif (r in range(232, 256) and g in range(134, 190) and
+                b in range(74, 119)):
             return Piece('L')
-        elif color in ((153, 127, 255), (92, 71, 190)):
+        elif (r in range(92, 154) and g in range(71, 128) and
+                b in range(190, 256)):
             return Piece('J')
-        elif color in ((255, 128, 255), (195, 74, 182)):
+        elif (r in range(195, 256) and g in range(74, 129) and
+                b in range(182, 256)):
             return Piece('T')
-        elif color in ((255, 255, 118), (236, 206, 76)):
+        elif (r in range(236, 256) and g in range(206, 256) and
+                b in range(76, 119)):
             return Piece('O')
         else:
             # print("nuevo color: " + str(color))
