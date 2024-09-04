943761c70424423970a8c25b386d1aa2c7346b91
brrodriguezd <brrodriguezd@unal.edu.co>
2024-09-04 09:46:16 -0500

    soy el mejor

--- cuadrito/agente.js
+++ cuadrito/agente.js
@@ -116,13 +116,20 @@ class Individuo extends Agent {
     // O(length¹ * limit^profundidad)
     //limit = profundidad_root(1/length²) * c
     let moves = this.board.valid_moves(board);
+    let computacion = 100000000;
     this.limit = board.length > 30 ? 30 : board.length < 10 ? 10 : board.length;
-    console.log(moves.length);
-    console.log(board.length);
-    console.log(moves.length * board.length);
+    console.log(
+      Math.floor(
+        Math.abs(Math.log(computacion / Math.pow(board.length, 4))) -
+          Math.abs(Math.log(this.limit))
+      ) + 1
+    );
+    this.profundidad =
+      Math.floor(
+        Math.abs(Math.log(computacion / Math.pow(board.length, 4))) -
+          Math.abs(Math.log(this.limit))
+      ) + 1;
     if (time < 1000) {
-      return moves[Math.floor(Math.random() * moves.length)];
-    } else if (moves.length * board.length > 300000) {
       return moves[Math.floor(Math.random() * moves.length)];
     }
     let alpha = -Infinity;
